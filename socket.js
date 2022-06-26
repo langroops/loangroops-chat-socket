4546c0ba3a41b74506be2497a880798d42c4c08b
langroops <langroops@gmail.com>
2022-06-26 14:53:21 -0400

    additional cors

--- socket.js
+++ socket.js
@@ -3,12 +3,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const cors = require("cors");
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
+
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
 
 io.on("connection", (socket) => {
   console.log("a user connected");
