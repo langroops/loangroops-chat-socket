47fc29016ddd3cf65478cf53209a3f32db54d299
langroops <langroops@gmail.com>
2022-06-26 13:30:12 -0400

    beta v. 1.0

--- socket.js
+++ socket.js
@@ -13,12 +13,12 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("join", (channelId) => {
+  socket.on("join-channel", (channelId) => {
     socket.join(channelId);
     console.log("user joined channel " + channelId);
   });
 
-  socket.on("leave", (channelId) => {
+  socket.on("leave-channel", (channelId) => {
     socket.leave(channelId);
     console.log("user left channel " + channelId);
   });
@@ -26,6 +26,57 @@ io.on("connection", (socket) => {
   socket.on("typing", (channelId, senderName) => {
     console.log("typing", channelId, senderName);
     socket.to(channelId).emit("typing", `${senderName} is typing...`);
+  });
+
+  socket.on("join-audio-room", (channelId, userId) => {
+    socket.join("audio-" + channelId);
+    console.log("user joined audio " + channelId);
+    io.to("audio-" + channelId).emit(
+      "user-joined-audio-room",
+      userId,
+      socket.id
+    );
+    socket.on("disconnect", () => {
+      io.to("audio-" + channelId).emit("user-left-audio-room", userId);
+    });
+  });
+
+  socket.on("user-in-room", (channelId, userId, socketId, userActive) => {
+    console.log("other user in room", userId);
+    socket.to(socketId).emit("other-user-in-room", userId, userActive);
+  });
+
+  socket.on("join-call", (channelId, userId, peerId) => {
+    io.to("audio-" + channelId).emit(
+      "user-joined-call",
+      channelId,
+      userId,
+      peerId
+    );
+
+    // socket.on("disconnect", () => {
+    //   socket.to("audio-" + channelId).emit("user-left-call", userId);
+    // });
+  });
+
+  socket.on("leave-audio-room", (channelId, userId) => {
+    socket.leave("audio-" + channelId);
+    //  socket.removeAllListeners("disconnect");
+    socket.to("audio-" + channelId).emit("user-left-audio-room", userId);
+    console.log("user left audio " + channelId);
+  });
+
+  socket.on("leave-call", (channelId, userId, peerId) => {
+    io.to("audio-" + channelId).emit(
+      "user-left-call",
+      channelId,
+      userId,
+      peerId
+    );
+  });
+
+  socket.on("disconnet", () => {
+    socket.removeAllListeners();
   });
 });
 
