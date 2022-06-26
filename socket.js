0da2f9876fd49fb4e02bf38dd56c2a5db05950f8
langroops <langroops@gmail.com>
2022-06-26 14:40:07 -0400

    wildcard

--- socket.js
+++ socket.js
@@ -6,8 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://langroops-chat.onrender.com/"],
-    credentials: true,
+    origin: "*",
   },
 });
 
