90c89926e6a502880f9b2290b9797eba79d6f4a5
langroops <langroops@gmail.com>
2022-06-26 13:44:28 -0400

    port modification

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://langroops-chat.onrender.com/"],
   },
 });
 
@@ -80,6 +80,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
   console.log("listening on *:3001");
 });
